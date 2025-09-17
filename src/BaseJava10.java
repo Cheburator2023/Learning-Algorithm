5589da99515cd28818e8ba7afb7fb0b112fdc3dd
Виктор <borisborisov03102001@gmail.com>
2025-09-17 19:57:19 +0300

    Commit 10

--- src/BaseJava10.java
+++ src/BaseJava10.java
@@ -1,18 +1,85 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class BaseJava10 {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,8,9,10,0,1,2,3};
-        System.out.println(search(nums, 0));
-        System.out.println(5^8);
-        System.out.println(1^3);
-        System.out.println(6^4);
+        int[] nums = {4, 5, 6, 7, 8, 9, 10, 0, 1, 2, 3};
+        int[][] intervals = {{1, 3}, {0, 9}, {8, 10}, {15, 18}};
+        int[][] intervals1 = {{0, 4}, {1, 5}, {8, 10}, {15, 18}};
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 20;
+//        System.out.println(search(nums, 0));
+//        printArrays(merge(intervals));
+//        System.out.println();
+//        printArrays(merge(intervals1));
+        System.out.println(searchMatrix1(matrix,target));
     }
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1) {
+            return false;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean searchMatrix1(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1) {
+            return false;
+        }
+
+        int row = 0, col = matrix[0].length - 1;
+
+        while (0 <= row && row <= matrix.length - 1 && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] > target) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][];
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
+        result.add(current);
+        for (int[] interval : intervals) {
+            if (interval[0] <= current[1]) { // перекрывается ли текущий интервал из результата с рассматриваемым интервалом
+                // если начало нового интервала меньше или равно концу текущего, значит, они перекрываются
+                current[1] = Math.max(current[1], interval[1]); // расширяем конец текущего интервала только если новый интервал заканчивается позже
+            } else {
+                current = interval;
+                result.add(current);
+            }
+        }
+        return result.toArray(new int[(result.size())][]);
+    }
+
+    private static void printArrays(int[][] nums) {
+        for (int[] i : nums) {
+            System.out.print(Arrays.toString(i));
+        }
+    }
+
     public static int missingNumber(int[] nums) {
-        if(nums == null) {
+        if (nums == null) {
             return -1;
         }
         int n = nums.length;
@@ -22,7 +89,7 @@ public class BaseJava10 {
     }
 
     public static int missingNumber1(int[] nums) {
-        if(nums == null || nums.length <= 1) {
+        if (nums == null || nums.length <= 1) {
             return -1;
         }
         Set<Integer> set = new HashSet<>();
@@ -31,7 +98,7 @@ public class BaseJava10 {
         }
 
         for (int i = 0; i < nums.length + 1; i++) {
-            if(set.add(i)) {
+            if (set.add(i)) {
                 return i;
             }
         }
@@ -54,7 +121,7 @@ public class BaseJava10 {
             return -1;
         }
         int left = 0, right = nums.length - 1;
-        while(left<=right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
